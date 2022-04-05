f2c82748108567e8ff04df4787a7338c34c2526d
Duleendra Shashimal <duleendra@bambu.co>
2022-04-06 00:30:39 +0800

    Datadog lambda integration stack

--- lib/datadog-lambda-stack.ts
+++ lib/datadog-lambda-stack.ts
@@ -1,16 +1,69 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {Stack, StackProps} from 'aws-cdk-lib';
+import {Construct} from 'constructs';
+import {Rule, Schedule} from "aws-cdk-lib/aws-events";
+import {Code, Function, FunctionBase, Runtime} from "aws-cdk-lib/aws-lambda";
+import * as path from "path";
+import {LambdaFunction} from "aws-cdk-lib/aws-events-targets";
+import {Datadog} from "datadog-cdk-constructs-v2";
 
 export class DatadogLambdaStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        //Customer API Lambda
+        const customerApi = new Function(this, 'CustomerAPI', {
+            functionName: 'Customer-API',
+            runtime: Runtime.NODEJS_14_X,
+            memorySize: 512,
+            handler: 'index.handler',
+            code: Code.fromAsset(path.join(__dirname, '../lambda/customer')),
+            environment: {
+                DD_ENV: 'prod',
+                DD_SERVICE: 'Customer-Service',
+                DD_VERSION: '1',
+                DD_TAGS:'name:customer-api'
+            }
+        })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DatadogLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        //Order API Lambda
+        const orderApi = new Function(this, 'OrderAPI', {
+            functionName: 'Order-API',
+            runtime: Runtime.NODEJS_14_X,
+            memorySize: 512,
+            handler: 'index.handler',
+            code: Code.fromAsset(path.join(__dirname, '../lambda/order')),
+            environment: {
+                DD_ENV: 'prod',
+                DD_SERVICE: 'Order-Service',
+                DD_VERSION: '1',
+                DD_TAGS:'name:order-api'
+            }
+        })
+
+        //Datadog integration
+        const dataDog = new Datadog(this, "DataDogIntegratoin", {
+            nodeLayerVersion: 74,
+            extensionLayerVersion: 21,
+            apiKey: 'fefec54589a5622b9f2c0e97889558d3',
+            site: 'datadoghq.com'
+        });
+
+        dataDog.addLambdaFunctions([customerApi,orderApi]);
+
+        //Added this for testing
+        //Event rule which runs the job every five minutes
+        const cronRuleCustomer = new Rule(this, 'cronRuleCustomer', {
+            schedule: Schedule.expression('cron(0/2 * * * ? *)'),
+            enabled: true
+        });
+
+        const cronRuleOrder = new Rule(this, 'CustomerOrder', {
+            schedule: Schedule.expression('cron(0/2 * * * ? *)'),
+            enabled: true
+        });
+
+        //Trigger the lambda functions
+        cronRuleCustomer.addTarget(new LambdaFunction(customerApi));
+        cronRuleOrder.addTarget(new LambdaFunction(orderApi));
+    }
 }
